0d781c4acc00a5e13e35ed8ed699f2f3f2005bd6
Heber <hebergonza@gmail.com>
2020-01-23 22:23:42 -0500

    created dashboard.js, display.js

---
+++ my-app/src/components/Dashboard.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <section>
+      <button>strike</button>
+      <button>ball</button>
+      <button>foul</button>
+      <button>hit</button>
+    </section>
+  )
+};
+
+export default Dashboard;
---
+++ my-app/src/components/Display.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Display = () => {
+  return (
+    <section>
+      <h1>At Bat</h1>
+      <h2 className="balls">Balls: </h2>
+      <h2 className="strikes">Strikes: </h2>
+    </section>
+  )
+};
+
+export default Display;
